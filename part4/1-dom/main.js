156c971045c0696ac22688faa08ae88fccd90a1e
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-07-28 15:39:44 +0330

    add dom

---
+++ part4/1-dom/main.js
@@ -0,0 +1,11 @@
+// dom  is document object model
+
+// dom is a api for change html  with js
+
+// dom comes from window
+
+// window is global object in browser
+
+// dom is webApi
+console.log(window.document);
+console.log(window);
