d1cbf90849d6c4ff960dc5cce5473f2e1d15c9d2
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-07-28 15:31:00 +0330

    add prompt confirm alert

--- part3/1-date/main.js
+++ part3/1-date/main.js
@@ -27,7 +27,7 @@ console.log(milisecond);
 
 // index of month  and  day starts from zero 
 
-// sunday-monday-tuesday-wednesday-thursday-friday-sayurday
+// sunday-monday-tuesday-wednesday-thursday-friday-saturday
 // 0-1-2-3-4-5-6
 
 // january=0,
---
+++ part3/7-promptalertconfirm/main.js
@@ -0,0 +1,16 @@
+
+// prompt is a method in widndow actualy a webAPI from window.window is global object for browser.
+
+// if we get a data from user ,use prompt
+
+// example
+
+
+const userName=prompt("please enter your name");
+console.log(userName);/*  user type or entry */
+
+const age=confirm("are you a 18?");
+console.log(age); /* boolean  */
+
+
+// alert("welcome") /* open a modal on screen for show a message for user */
